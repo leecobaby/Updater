24b7e849df1cf2a4d733043348cb20ea03c9ba8b
Leeco <leeco19@qq.com>
2021-06-14 19:21:12 +0800

    fix jd_zoo getFeedDetail  error

--- html/jd_zoo.js
+++ html/jd_zoo.js
@@ -183,7 +183,7 @@ function doTask () {
     return
   }
 
-  if ([1, 2, 3, 5, 7, 9, 26].includes($.oneTask.taskType) && $.oneTask.status === 1) {
+  if ([1, 2, 3, 5, 7, 9, 26].includes($.oneTask.taskType) && $.oneTask.status === 1 && $.oneTask.taskName.includes("逛逛")) {
     $.activityInfoList = $.oneTask.productInfoVos || $.oneTask.shoppingActivityVos || $.oneTask.brandMemberVos || $.oneTask.followShopVo || $.oneTask.browseShopVo;
 
     oneActivityInfo()
