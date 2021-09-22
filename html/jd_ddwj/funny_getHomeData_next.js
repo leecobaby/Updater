9c29aaee3b937dbfddb93af52dd53acfbf1bca0f
Leeco <leeco19@qq.com>
2021-09-22 14:07:01 +0800

    test

--- html/jd_ddwj/funny_getHomeData_next.js
+++ html/jd_ddwj/funny_getHomeData_next.js
@@ -1,8 +1,7 @@
-// next
 $.callback = ''
 dealReturn('funny_getHomeData', $.data)
 $.userInfo = $.homeData.result.homeMainInfo
 $.success = 1
-$.message = `当前分红：${$.userInfo.raiseInfo.redNum}份，当前等级:${$.userInfo.raiseInfo.scoreLevel}\n当前金币${$.userInfo.raiseInfo.remainScore}，下一关需要${$.userInfo.raiseInfo.nextLevelScore - $.userInfo.raiseInfo.curLevelStartScore}`
+$.message = `当前已解锁宝物进度:${$.userInfo.raiseInfo.userEarthInfo.userUnlockedPlaceNum}/15\n剩余好玩豆${$.userInfo.raiseInfo.remainScore}，下一关需要${$.userInfo.raiseInfo.nextLevelScore - $.userInfo.raiseInfo.curLevelStartScore}`
 document.write(JSON.stringify($))
 console.log($.message);
