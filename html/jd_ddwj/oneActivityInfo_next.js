93ae36edc073070bd594e265ab9c1bff5ffb412a
Leeco <leeco19@qq.com>
2021-09-23 02:11:37 +0800

    更新 京东-东东玩家 做任务功能 已正常运行

--- html/jd_ddwj/oneActivityInfo_next.js
+++ html/jd_ddwj/oneActivityInfo_next.js
@@ -16,7 +16,7 @@ if ($.callbackInfo.code === 0 && $.callbackInfo.data?.result?.taskToken) {
     $.callback = ''
     $.wait = 1
     $.success = 1
-    $.message = `完成任务： ${$.data.result?.successToast}`
+    $.message = `完成任务： ${$.data?.data?.result?.successToast}`
     console.log($.message)
     document.write(JSON.stringify($))
   }
--- html/jd_ddwj/oneActivityInfo_next_next.js
+++ html/jd_ddwj/oneActivityInfo_next_next.js
@@ -2,7 +2,7 @@ if (!document.body.innerText) {
   $.callback = ''
   $.wait = 1
   $.success = 1
-  $.message = `完成任务： ${$.data.result?.successToast}`
+  $.message = `完成任务： ${$.data?.data?.result?.successToast}`
   console.log($.message)
   document.write(JSON.stringify($))
 }
