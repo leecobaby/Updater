e3ee17ad523d50776d6ac4281c6b8d40006fbb58
Leeco <leeco19@qq.com>
2021-09-23 09:04:57 +0800

    更新 京东 pv 活动链接

--- html/jd_ddwj.js
+++ html/jd_ddwj.js
@@ -145,7 +145,7 @@ function oneActivityInfo () {
       $.callback = ''
       $.wait = 1
       $.success = 1
-      $.message = `完成任务： ${$.data?.data?.result?.successToast}`
+      $.message = `${$.data?.data?.result?.successToast}`
       console.log($.message)
       document.write(JSON.stringify($))
     }
