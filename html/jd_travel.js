2d03de3061d33638713bfed5070c499ced80b9bd
Leeco <leeco19@qq.com>
2021-10-24 20:11:15 +0800

    update

--- html/jd_travel.js
+++ html/jd_travel.js
@@ -180,7 +180,7 @@ function pkHelp () {
 
 // pk助力
 function travel_pk_collectPkExpandScore () {
-  if (new Date().getHours() >= 8 && new Date().getHours() <= 19) {
+  if (new Date().getHours() >= 20 && new Date().getHours() <= 23) {
     $.callback = 'Func.request'
     $.pkExpandId = 'PKASTT0195L6r47PBTNYCtIMjDX0CjRWnIaRzT0jeQO' || 'PKASTT0195L6r47PBTNYCtIMjDX0CjRWnIaRzTIjeQOc'
     takePostRequest('travel_pk_collectPkExpandScore');
