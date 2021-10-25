39f9d68fe6e5cf6c01dbdab2f20eb852c146c1f8
Leeco <leeco19@qq.com>
2021-10-25 17:05:31 +0800

    fix bug

--- html/jd_travel.js
+++ html/jd_travel.js
@@ -940,7 +940,7 @@ function dealReturn (type, data) {
       data = JSON.parse(data)
       $.data = {}
       // 选出有 助力码 的元素
-      const filterData = _.filter(data.items, v => v.text.match(/^[\w-]{20,}$/g))
+      const filterData = _.filter(data.items, v => v.text.match(/^[\w-]{15,}$/g))
       // 过滤重复的 user id
       const uniqData = _.uniqBy(filterData, v => v.fromUser)
       // 随机选取出 5 个助力码 - 考虑到助力已满情况和无效码的情况
