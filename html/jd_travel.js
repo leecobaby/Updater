34b1da253038148d9835a70c3e04f0309787d6ef
Leeco <leeco19@qq.com>
2021-10-28 14:13:31 +0800

    fix bug

--- html/jd_travel.js
+++ html/jd_travel.js
@@ -158,7 +158,6 @@ function travel_sign () {
   // next next
   if (!document.body.innerText) {
     $.callback = ''
-    $.next = 0 // 覆盖前面的 0 
     dealReturn('travel_getSignHomeData', $.data)
     document.write(JSON.stringify($))
   }
--- html/jd_travel/travel_sign_next_next.js
+++ html/jd_travel/travel_sign_next_next.js
@@ -1,7 +1,6 @@
 // next next
 if (!document.body.innerText) {
   $.callback = ''
-  $.next = 0 // 覆盖前面的 0 
   dealReturn('travel_getSignHomeData', $.data)
   document.write(JSON.stringify($))
 }
