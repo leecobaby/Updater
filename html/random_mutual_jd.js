87eb80b9b60dc14cd57f2660726222b85b293a57
Leeco <leeco19@qq.com>
2021-10-30 10:06:55 +0800

    update random

--- html/random_mutual_jd.js
+++ html/random_mutual_jd.js
@@ -1,7 +1,7 @@
 // const json = 
 const data = json.items
 // 选出有 url 的元素
-const filterData = _.filter(data, v => v.text.match(/:\/\W[A-Za-z0-9_]{10,15}\W/g))
+const filterData = _.filter(data, v => v.text.match(/\W[A-Za-z0-9]{10,15}\W/g))
 // 过滤重复的 user id
 const uniqData = _.uniqBy(filterData, v => v.fromUser)
 // 随机选取出 5 个助力 url - 考虑到助力已满情况和无效链接的情况
