fe56bdb69b4ee25a80ea37709902715069d76b76
Leeco <leeco19@qq.com>
2021-10-31 15:45:37 +0800

    fix bug

--- html/tb/getCookieData.js
+++ html/tb/getCookieData.js
@@ -1,6 +1,6 @@
 // const cookieStr = ``
 // const activityId = ``
 const Cookie = cookieStr.replace(/\s/g, '')
-const tk = Cookie.match(/_m_h5_tk=\w+/g)[0]
+const tk = Cookie.match(/_m_h5_tk=(\w+)/)[1]
 const url = `https://service-lv90ws2p-1251309300.sh.apigw.tencentcs.com/release/api?activityId=${activityId}&tk=${tk}`
 document.write(JSON.stringify({ Cookie, url }))
