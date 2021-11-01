5d128cf052b0d5edfb11327c721c6db8423bf535
Leeco <leeco19@qq.com>
2021-11-01 17:15:27 +0800

    fix bug

--- html/jd_travel_development.js
+++ html/jd_travel_development.js
@@ -989,7 +989,7 @@ function getPostBody (type) {
 
 // 处理返回信息
 function dealReturn (type, data) {
-  if (!data) $.error = '接口返回数据为空，检查账号cookie是否过期或错误';
+  if (!data.d) $.error = '接口返回数据为空，检查账号cookie是否过期或错误';
   // 对 15.1 的特殊优化
   $.data = JSON.parse(data.d)
   data = $.data
