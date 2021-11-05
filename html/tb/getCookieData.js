3ed64b3f31270c3d101b12dba2bc5770957ebeaa
Leeco <leeco19@qq.com>
2021-11-05 15:02:57 +0800

    update 1111 data

--- html/tb/getCookieData.js
+++ html/tb/getCookieData.js
@@ -1,6 +1,11 @@
 // const cookieStr = ``
 // const activityId = ``
 const Cookie = cookieStr.replace(/\s/g, '')
-const tk = Cookie.match(/_m_h5_tk=(\w+)/)[1]
-const url = `https://service-lv90ws2p-1251309300.sh.apigw.tencentcs.com/release/api?activityId=${activityId}&tk=${tk}`
-document.write(JSON.stringify({ Cookie, url }))
+
+try {
+  const tk = Cookie.match(/_m_h5_tk=(\w+)/)[1]
+  const url = `https://service-lv90ws2p-1251309300.sh.apigw.tencentcs.com/release/api?activityId=${activityId}&tk=${tk}`
+  document.write(JSON.stringify({ Cookie, url }))
+} catch (error) {
+  document.write(JSON.stringify({ error }))
+}
