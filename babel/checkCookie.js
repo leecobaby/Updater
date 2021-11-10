f92e5818e81777314445784e62566955afa52d45
Leeco <leeco19@qq.com>
2021-11-10 20:23:21 +0800

    fix bug

---
+++ babel/checkCookie.js
@@ -0,0 +1,21 @@
+// const data = {}
+const $ = {}
+if (data.retcode === 1001) {
+  $.overdue = 'Cookie 已过期，请去往指令设置重新授权登录！'
+}
+else {
+  $.nickname = data.data?.userInfo?.baseInfo?.nickname
+  $.beanNum = data.data?.assetInfo?.beanNum
+}
+document.write(JSON.stringify($))
+
+function isJsonString (str) {
+  try {
+    if (typeof JSON.parse(str) == "object") {
+      return true;
+    }
+  } catch (e) {
+  }
+  return false;
+}
+
