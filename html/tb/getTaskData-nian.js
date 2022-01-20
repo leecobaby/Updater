2ed46bf4912669046d1bcc873cb48e01905e67b0
Leeco <leeco19@qq.com>
2022-01-20 19:17:14 +0800

    tb FarmSingle - update logic

--- html/tb/getTaskData-nian.js
+++ html/tb/getTaskData-nian.js
@@ -85,7 +85,7 @@ function taskHandle (data) {
         for (let i = 0; i < times; i++) {
           let deliveryId = item.taskParams.deliveryId
           if (deliveryId == '15901' || deliveryId == '18735' || deliveryId == '23176') continue;
-          let title = item.assets.title
+          let title = item.assets && item.assets.title || 'null'
           let implId = item.taskParams.implId.match(/(.*)_/g) + i
           fromToken = item.taskParams.fromToken
           items.push(`${deliveryId}-${title} fromToken=${fromToken}&deliveryId=${deliveryId}&implId=${implId}`)
