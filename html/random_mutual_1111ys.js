ece962af37660de0b96fb128b09eb2030fbe65ca
Leeco <leeco19@qq.com>
2022-05-24 23:14:34 +0800

    update 1111ys help code

--- html/random_mutual_1111ys.js
+++ html/random_mutual_1111ys.js
@@ -4,8 +4,8 @@ const data = json.items
 const filterData = _.filter(data, v => v.urls[0])
 // 过滤重复的 user id
 const uniqData = _.uniqBy(filterData, v => v.fromUser)
-// 随机选取出 2 个助力 url - 考虑到助力已满情况和无效链接的情况
-const sampleData = _.sampleSize(uniqData, 2)
+// 随机选取出 3 个助力 url - 考虑到助力已满情况和无效链接的情况
+const sampleData = _.sampleSize(uniqData, 3)
 const urls = sampleData.map(v => v.urls[0].url)
 
 // const item = urls.map(v => '1 ' + v.match(/https:\/\/(.*)/)[1] + '&1');
