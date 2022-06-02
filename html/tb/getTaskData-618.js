35c9ee882ef3ae532eb42c50ecbe1cb1dbf7753f
Leeco <leeco19@qq.com>
2022-06-02 20:08:59 +0800

    update 618cat data

--- html/tb/getTaskData-618.js
+++ html/tb/getTaskData-618.js
@@ -15,6 +15,8 @@ const firstTask = [
       "urlScheme": `HTTPS://t.youku.com/yep/page/m/stghkpyvr1?isNeedBaseImage=1&from=15yuevip&prismChannel=poplayer&adTrace=314694502350001__user_define.browse__21206dac16541452137987458e6f20__I__H__0&adScene=2022618-task-list&fromToken=${fromToken}&spm=a217e.xzrwy.1.1&sceneId=3399&sourceType=other&hd_from_id=100155&deliveryId=`,
       "textEnd": "str1&implId=str2",
       "item": [
+        "29598 cloudsail_0_318392501470001_29598_0",
+        "29598 cloudsail_0_318392501470002_29598_0",
         "29239 cloudsail_7_319269003870001_29239_0",
         "29239 cloudsail_7_319269003870002_29239_0",
         "29239 cloudsail_7_319269003870003_29239_0",
@@ -42,7 +44,34 @@ const firstTask = [
         "28572 cloudsail_14_316722004310009_28572_0",
         "28572 cloudsail_14_316722004310000_28572_0",
       ]
+    },
+    {
+    "stars": {
+      "title": "去往淘宝",
+      "type": "stars",
+      "urlScheme": "HTTPS://pages.tmall.com/wow/z/hdwk/20220618/gamehome?disableNav=YES&qd_from=zfbbanner1&bc_fl_src=zfb_banner1&prismFrom=zfb&slk_gid=gid_er_normal&hd_from_id=100155&sceneId=3399&deliveryId=",
+      "textEnd": "str1&implId=str2",
+      "item": ["28911 other_0_918001_28911_0"]
     }
+  },
+  {
+    "stars": {
+      "title": "惊喜礼盒 无支付宝请取消勾选",
+      "type": "stars",
+      "urlScheme": "alipays://platformapi/startapp?appId=68687592&url=https%3A%2F%2Frender.alipay.com%2Fp%2Fyuyan%2F180020380000000087%2Findex.html%3FcaprMode%3Dsync%26chInfo%3DTAOBAO_BOX&titlePenetrate=YES&ttb=always&chInfo=TAOBAO_BOX&abv=NO&transparentTitle=always",
+      "textEnd": "",
+      "item": ["1 0"]
+    }
+  },
+  {
+    "stars": {
+      "title": "前往支付宝 无支付宝请取消勾选",
+      "type": "stars",
+      "urlScheme": "alipays://platformapi/startapp?appId=68687592&url=https%3A%2F%2Frender.alipay.com%2Fp%2Fyuyan%2F180020380000000087%2Findex.html%3FcaprMode%3Dsync%26chInfo%3DTAOBAO_TASK&titlePenetrate=YES&ttb=always&chInfo=TAOBAO_TASK&abv=NO&transparentTitle=always",
+      "textEnd": "",
+      "item": ["1 0"]
+    }
+  }
   }
 ];
 
@@ -77,7 +106,7 @@ function taskHandle (data) {
 function taskBaseData () {
   return {
     "618cat": {
-      "version": "数据最后更新于:5.29.1",
+      "version": "数据最后更新于:6.2.2",
       "pv": {
         "taobao": "taobao://m.taobao.com/tbopen/index.html?h5Url=https://m.tb.cn/h.ftJSz3R",
         "tmall": "tmall://page.tm/appLink?&action=ali.open.nav&h5Url=https%3A%2F%2Fm.tb.cn%2Fh.ftJSz3R",
