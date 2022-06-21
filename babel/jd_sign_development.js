0009add71b3e38da0e213b2a4737e96cea6d1440
Leeco <leeco19@qq.com>
2022-06-21 15:18:32 +0800

    test

--- babel/jd_sign_development.js
+++ babel/jd_sign_development.js
@@ -1352,7 +1352,7 @@ function takeRequest (type) {
       myRequest = getRequest(url, body);
       break;
     case 'oneActivityInfo':
-      url = `https://api.m.jd.com/client.action?functionId=receiveNutrientsTask&body=${encodeURIComponent(JSON.stringify({ "awardType": $.oneTask.taskType, "monitor_refer": "receiveNutrientsTask" }))}&appid=ld&client=apple&area=19_1601_50258_51885&build=167490&clientVersion=9.3.2`;
+      url = `https://api.m.jd.com/client.action?functionId=receiveNutrientsTask&body=${encodeURIComponent(JSON.stringify({ "awardType": $.oneTask.taskType, "monitor_refer": "receiveNutrientsTask", "monitor_source": "plant_app_plant_index", "version": "9.2.4.1" }))}&appid=ld&client=apple&area=19_1601_50258_51885&build=167490&clientVersion=9.3.2`;
       myRequest = getRequest(url, body, 'GET');
       break;
     default:
