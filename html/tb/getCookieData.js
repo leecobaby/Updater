4cf623ae02130926601bea272b5c3615bfd76e3c
Leeco <leeco19@qq.com>
2022-07-05 21:45:43 +0800

    update tb logic

--- html/tb/getCookieData.js
+++ html/tb/getCookieData.js
@@ -1,14 +1,12 @@
 // const cookieStr = ``
 // const activityId = ``
-// const activityId2 = `` // 只针对淘金币新旧任务能同时做时
 
 const Cookie = cookieStr.replace(/\s/g, '')
 
 try {
   const tk = Cookie.match(/_m_h5_tk=(\w+)/)[1]
   const url = `https://service-lv90ws2p-1251309300.sh.apigw.tencentcs.com/release/api?activityId=${activityId}&tk=${tk}`
-  const url2 = `https://service-lv90ws2p-1251309300.sh.apigw.tencentcs.com/release/api?activityId=${activityId2}&tk=${tk}`
-  document.write(JSON.stringify({ Cookie, url, url2 }))
+  document.write(JSON.stringify({ Cookie, url }))
 } catch (error) {
   document.write(JSON.stringify({ error: `Cookie错误，请检查！` }))
 }
